707da9b8a8de814196f7bb1518857a5a10efa08b
Cynthia Deng <cynthiad215@gmail.com>
2018-09-15 19:14:01 -0700

    ADD template for frontend vue

--- ncs/_assets/js/app.js
+++ ncs/_assets/js/app.js
@@ -1,13 +1,29 @@
 var app = new Vue({
     el: '#app',
     data: {
-      greeting: 'Welcome to Shareit/Sharespace/Not Reddit',
+        greeting: 'Welcome to Shareit/Sharespace/Not Reddit',
+        categories: {
+            dog: 1,
+            cat: 2,
+        },
+        data: []
     },
+
+    created: function() {
+        this.fetchData()
+    },    
+    
     methods: {
-      humanizeURL: function (url) {
-        return url
-          .replace(/^https?:\/\//, '')
-          .replace(/\/$/, '')
-      }
-    }
+        fetchData: function() {
+            console.log("hey")
+			var xhr = new XMLHttpRequest()
+			var self = this
+			xhr.open('GET', 'https://c5102e1b.ngrok.io/api/posts')
+			xhr.onload = function() {
+				self.data = JSON.parse(xhr.responseText)
+                console.log(self.data)
+			    }
+			xhr.send()
+		    }
+	    }
   })
