68b51f9185bbcfa20e78c3c4c77cc5ce16e080cc
Nicholas C <nick.chim95@gmail.com>
2018-09-16 02:11:23 -0700

    Commit GET user saved posts endpoint

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ router.post("/user/auth", function(req, res) {
 
     let queryString = `SELECT u.UserId, u.Username, b.Title
                       FROM dbo.[User] u, dbo.Badge b
-                      LEFT JOIN dbo.UserBadge ub
+                      INNER JOIN dbo.UserBadge ub
                       ON UserId = ub.UserId
                       WHERE u.Username = @username AND u.Password = @password`;
 
@@ -58,15 +58,15 @@ router.post("/user/auth", function(req, res) {
           return res.sendStatus(500);
         }
 
-        if (result.recordsets.length < 1) {
-          res.status(403).send({
-            "Error": "No user found matching that Username/Password"
-          });
-        }
         ps.unprepare(function(err) {
           if (err) {
             console.log("encountered an error with unpreparing statement");
-            return res.sendStatus(500);
+          }
+
+          if (result.recordset.length < 1) {
+            res.status(403).send({
+              "Error": "No user found matching that Username/Password"
+            });
           }
 
           return res.send({
@@ -88,20 +88,80 @@ router.post("/user/auth", function(req, res) {
   URL: https://c5102e1b.ngrok.io/api/user/saved
  */
 router.get("/user/saved", function(req, res) {
-  //stub
-  let sampleResObj = {
-    "posts": [
-      {
-        "PostId": "61ccbf82-4061-48b9-be7b-bf1e7f84bc07",
-        "Link": "https://www.theglobeandmail.com/opinion/article-how-many-people-actually-suffer-from-mental-illness/",
-        "Title": "How many people actually suffer from mental illness?",
-        "ImageUrl": null,
-        "Kudos": 43,
-        "BadgeName": "RoboCop"
+  getPool().then(function(pool) {
+    if (!req.query.UserId) return res.sendStatus(400);
+
+    let queryString = `SELECT p.PostId, c.Title cTitle, p.DateCreated, p.Link, p.Title pTitle, p.ImageUrl, p.Kudos, b.Title bTitle
+                       FROM dbo.SavedPost sp
+                       INNER JOIN dbo.Post p
+                       ON sp.PostId = p.PostId
+                       INNER JOIN dbo.Category c
+                       ON p.CategoryId = c.CategoryId
+                       INNER JOIN dbo.[User] u
+                       ON p.UserId = u.UserId
+                       INNER JOIN dbo.UserBadge ub
+                       ON u.UserId = ub.UserId
+                       INNER JOIN dbo.Badge b
+                       ON ub.BadgeId = b.BadgeId
+                       WHERE sp.UserId = @userid`
+
+    let ps = new sql.PreparedStatement(pool);
+    ps.input("userid", sql.UniqueIdentifier);
+    ps.prepare(queryString, function(err, result) {
+      if (err) {
+        console.log("couldn't prepare statement");
+        return res.sendStatus(500);
       }
-    ]
-  }
-  return res.send(sampleResObj);
+
+      let paramsObj = {
+        "userid": req.query.UserId
+      }
+
+      ps.execute(paramsObj, function(err, result) {
+        if (err) {
+          console.log("encountered an error with executing query");
+          return res.sendStatus(500);
+        }
+
+        if (result.recordsets.length < 1) {
+          res.status(403).send({
+            "Error": "No user found matching that Username/Password"
+          });
+        }
+        ps.unprepare(function(err) {
+          if (err) {
+            console.log("encountered an error with unpreparing statement");
+          }
+          
+          if (result.recordset.length < 1) {
+            res.status(403).send({
+              "Error": "No saved posts found for this user"
+            });
+          }
+
+          let returnObj = {};
+          returnObj.Posts = [];
+          result.recordset.forEach(function(record) {
+            returnObj.Posts.push({
+              "PostId": record.PostId,
+              "PostCategory": record.cTitle,
+              "DateCreated": record.DateCreated,
+              "Link": record.Link,
+              "Title": record.pTitle,
+              "ImageUrl": record.ImageUrl,
+              "Kudos": record.Kudos,
+              "BadgeName": record.bTitle
+            });
+          });
+
+          return res.send(returnObj);
+        });
+      });
+    });
+  }).catch(function(err) {
+    console.log("encountered an error getting pool");
+    return res.sendStatus(500);
+  });
 });
 
 /*
